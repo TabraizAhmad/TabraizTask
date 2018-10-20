4691906b10d5797c9419470847e071dee6add09b
tabraiz_ahmad <tabraiz.ahmad95@yahoo.com>
2018-10-21 03:55:13 +0400

    injecting retrofit and deleting helper class

--- app/src/main/java/com/assignment/tasktabraiz/di/applicationDI/module/MoviesRepositoryModule.java
+++ app/src/main/java/com/assignment/tasktabraiz/di/applicationDI/module/MoviesRepositoryModule.java
@@ -1,13 +1,19 @@
 package com.assignment.tasktabraiz.di.applicationDI.module;
 
+import com.assignment.tasktabraiz.di.applicationDI.scope.TaskApplicationScope;
 import com.assignment.tasktabraiz.moviedetail.repository.MoviesRepository;
-import com.assignment.tasktabraiz.network.RetrofitHelper;
 import com.assignment.tasktabraiz.network.WebService;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-@Module
+import static com.assignment.tasktabraiz.common.Constants.BASE_URL;
+
+@Module(includes = NetworkModule.class)
 public class MoviesRepositoryModule {
     @Provides
     public MoviesRepository moviesRepository(WebService webService){
@@ -16,12 +22,20 @@ public class MoviesRepositoryModule {
     }
 
     @Provides
-    public WebService webService(RetrofitHelper retrofitHelper) {
-        return retrofitHelper.getWebService();
+    public WebService webService(Retrofit retrofit) {
+        return retrofit.create(WebService.class);
     }
 
     @Provides
-    public RetrofitHelper retrofitHelper() {
-        return new RetrofitHelper();
+    @TaskApplicationScope
+    public Retrofit retrofit(OkHttpClient okHttpClient) {
+
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
+
 }
